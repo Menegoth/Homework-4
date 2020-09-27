d6fd114df42f0e991425a5c1175e32f0855eb6da
Antonio <antonioareas@me.com>
2020-09-27 14:42:22 -0700

    Add project files.

---
+++ Part 1/MainWindow.xaml.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+/// Week 4		Homework 1
+/// File Name: MainWindow.xaml.cs
+/// @author: Antonio Monteiro
+/// Date:  September 27th 2020
+
+namespace Part_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void submitButton_Click(object sender, RoutedEventArgs e)
+        {
+
+            //intialize variables
+            double weight = Double.Parse(weightTextBox.Text);
+            double height = Double.Parse(heightTextBox.Text);
+            double bmi;
+
+            //calculate bmi and assign value
+            bmi = calculateBMI(weight, height);
+
+            //check and update screen
+            if (bmi < 15)
+            {
+                //update screen
+                bmiCanvas.Background = Brushes.Blue;
+                bmiLabel.Content = "You are below healthy BMI. (" + bmi + ")";
+
+            } else if (bmi > 26)
+            {
+                //update screen
+                bmiCanvas.Background = Brushes.Green;
+                bmiLabel.Content = "You are at a healthy BMI. (" + bmi + ")";
+
+            } else
+            {
+                //update screen
+                bmiCanvas.Background = Brushes.Yellow;
+                bmiLabel.Content = "You are above healthy BMI. (" + bmi + ")";
+
+            }
+
+        }
+
+        //calculate bmi function
+        public double calculateBMI(double weight, double height)
+        {
+
+            //declare bmi variable
+            double bmi;
+
+            //calculate bmi
+            bmi = (weight * 720.0) / (height * height);
+
+            //return bmi
+            return Math.Round(bmi, 2);
+
+        }
+    }
+}
---
+++ Part 2/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+/// Week 4		Homework 2
+/// File Name: Program.cs
+/// @author: Antonio Monteiro
+/// Date:  September 27th 2020
+
+namespace Part_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //variables
+            int heads = 0;
+            int tails = 0;
+
+            //ask for user input
+            Console.WriteLine("Choose heads(h) or tails(h) 8 times.");
+
+            //loop flip 8 times
+            for (int i = 0; i < 8; i++)
+            {
+
+                char coin = char.Parse(Console.ReadLine());
+
+                if (coin == 'h')
+                {
+                    heads++;
+                } else if (coin == 't')
+                {
+                    tails++;
+                } else
+                {
+                    Console.WriteLine("Invalid argument.");
+                    i--;
+                }
+            }
+
+            //output results
+            Console.WriteLine("Number of heads: " + heads);
+            Console.WriteLine("Number of tails: " + tails);
+            Console.WriteLine("Percent heads: " + (heads / 8.0) * 100);
+            Console.WriteLine("Percent tails: " + (tails / 8.0) * 100);
+
+        }
+    }
+}
